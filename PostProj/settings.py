5825f905de1115a443e170350f097976985e6fe7
Benie <davinci.monalissa@gmail.com>
2022-06-29 15:39:45 +0300

    redeploy

--- PostProj/settings.py
+++ PostProj/settings.py
@@ -82,8 +82,6 @@ TEMPLATES = [
     },
 ]
 
-django_heroku.settings(locals())
-
 WSGI_APPLICATION = 'PostProj.wsgi.application'
 
 
@@ -140,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
