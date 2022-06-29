41a50b038b8557872d59cd841b00a3ccdf1063fc
Benie <davinci.monalissa@gmail.com>
2022-06-29 15:51:43 +0300

    add post method

--- postApp/views.py
+++ postApp/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from postApp.serializer import BookSerializer
 from postApp.models import Book 
+from rest_framework import status
 
 # Create your views here.
 class OurBookLibrary(APIView):
@@ -11,3 +12,11 @@ class OurBookLibrary(APIView):
 		books = Book.objects.all()
 		serializers = BookSerializer(books,many=True)
 		return Response(serializers.data)
+
+
+	def post(self, request, format=None):
+    	serializers = BookSerializer(data=request.data)
+     	if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
